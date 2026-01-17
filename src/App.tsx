0910bf7fb2fee79efec9b4daa363cfa981c3be77
이종경 <gyoung3063413@naver.com>
2026-01-17 18:03:19 +0900

    feat: integrate BookmarkDropdown and BookmarkButton components into App and RepositoryCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import RepositorySearchWrapper from '@/search/containers/RepositorySearchWrapper'
+import { BookmarkDropdown } from './search/components/BookmarkDropdown'
+import BookmarkProvider from '@/search/components/BookmarkProvider'
 
 function App() {
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="mb-4 text-2xl font-bold">GitHub Repository Search</h1>
-      <RepositorySearchWrapper>
-        <RepositorySearchWrapper.Input />
-        <RepositorySearchWrapper.Result />
-      </RepositorySearchWrapper>
-    </main>
+    <BookmarkProvider>
+      <main className="container mx-auto p-4">
+        <div className="mb-4 flex items-center justify-between">
+          <h1 className="text-2xl font-bold">GitHub Repository Search</h1>
+          <BookmarkDropdown />
+        </div>
+        <RepositorySearchWrapper>
+          <RepositorySearchWrapper.Input />
+          <RepositorySearchWrapper.Result />
+        </RepositorySearchWrapper>
+      </main>
+    </BookmarkProvider>
   )
 }
 
